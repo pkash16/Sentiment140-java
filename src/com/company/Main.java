3f06ac478d6306ef2e8f172c264dd1b2815a0045
pkash16 <p.kash16@gmail.com>
2015-07-13 10:50:59 -0400

    added twitter4j and simple query search

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,37 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import twitter4j.*;
 
 public class Main {
 
     private final String USER_AGENT = "Mozilla/5.0";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //output
+        System.out.println("Enter a query: ");
+
+        String query_string = scanner.nextLine();
+
+
+        // The factory instance is re-useable and thread safe.
+        Twitter twitter = TwitterFactory.getSingleton();
+        Query query = new Query(query_string);
+        try {
+            QueryResult result = twitter.search(query);
+            for (Status status : result.getTweets()) {
+                System.out.println(status.getText());
+            }
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+
 	    Main http = new Main();
         try{
             http.sendPost();
@@ -67,7 +89,7 @@ public class Main {
         JSONObject json = new JSONObject();
         JSONArray array = new JSONArray();
         array.put(new JSONObject().put("text", "I love titanic."));
-        array.put(new JSONObject().put("text", "I hate titanic."));
+        array.put(new JSONObject().put("text", "I love that i hate this"));
         json.put("data",array);
 
         String urlParameters = json.toString();
