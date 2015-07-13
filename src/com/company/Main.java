d2158ae53f698e0c4aa67fdeb93988780c0ecc6f
pkash16 <p.kash16@gmail.com>
2015-07-13 09:22:44 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,106 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Main {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) {
+	    Main http = new Main();
+        try{
+            http.sendPost();
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+
+    }
+
+    private void sendGet() throws Exception {
+
+        String url = "http://www.sentiment140.com/api/classify?text=new+moon+is+awesome&query=new+moon";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+    private void sendPost() throws Exception {
+
+        String url = "http://www.sentiment140.com/api/bulkClassifyJson";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        JSONObject json = new JSONObject();
+        JSONArray array = new JSONArray();
+        array.put(new JSONObject().put("text", "I love titanic."));
+        array.put(new JSONObject().put("text", "I hate titanic."));
+        json.put("data",array);
+
+        String urlParameters = json.toString();
+
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+
+}
